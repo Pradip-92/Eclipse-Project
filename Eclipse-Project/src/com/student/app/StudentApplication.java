ec7e7e4237ab8c05ebb802abcf61890cd2dd14df
pradip shinde <pradipshinde013@gmail.com>
2025-02-16 13:35:45 +0530

    Initial Commit

---
+++ Eclipse-Project/src/com/student/app/StudentApplication.java
@@ -0,0 +1,8 @@
+package com.student.app;
+
+public class StudentApplication {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the main.............");
+		System.out.println("================: Student Application V1.0.0 :================");
+	}
+}
